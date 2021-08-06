403589cffe895b6845255db6a85655e04c765ee6
Aaron <aaron.latorre@outlook.com>
2021-08-06 16:28:27 -0500

    gallery navbar complete

---
+++ script.js
@@ -0,0 +1,12 @@
+// Display mobile navbar
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
+
