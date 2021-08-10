e19a5f6a6c53e53b9a9b45d356b3a8b67b9eaf43
Aaron <aaron.latorre@outlook.com>
2021-08-10 00:50:56 -0500

    php email feature added

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    $mailTo = "kane62_98@hotmail.com";
+    $headers = "From: ".email;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+
+    mail($mailTo, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
