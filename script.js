7e95479e5e46a90c9940484ac7f28c1ba7eb07cb
Aaron <aaron.latorre@outlook.com>
2021-08-10 00:18:21 -0500

    contact me form added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navList = document.getElementsByTagName('li');
 
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
@@ -9,4 +10,8 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+navList[0].addEventListener('click', mobileMenu);
+navList[1].addEventListener('click', mobileMenu);
+navList[2].addEventListener('click', mobileMenu);
+navList[3].addEventListener('click', mobileMenu);
 
